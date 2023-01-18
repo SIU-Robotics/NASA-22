9f6a3d08765a50ab478f509576caeff03e401bf3
Andrew Barnes <barnesandrew123@gmail.com>
2023-01-17 21:24:11 -0600

    Added light code and gitignore file

--- robo_interface/controller/urls.py
+++ robo_interface/controller/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('test/', views.test, name='index')
+	path('test_page/', views.test_page, name='test_page'),
+	path('test/', views.test, name='test'),
+	path('toggle_light/', views.toggle_light, name='toggle_light'),
 ]
 
--- robo_interface/controller/views.py
+++ robo_interface/controller/views.py
@@ -1,4 +1,5 @@
 import json
+from .i2c.light import turnOn, turnOff
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -9,6 +10,9 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     return render(request, 'controller/index.html')
 
+def test_page(request):
+    return render(request, 'controller/test_page.html')
+
 @csrf_exempt
 def test(request):
     try:
@@ -16,6 +20,20 @@ def test(request):
         answer = float(payload.get("n1")) + float(payload.get("n2"))
         response = JsonResponse({"answer": answer})
         return response
+    except Exception as e:
+        response = JsonResponse({"error": str(e)})
+        response.status_code = 500
+        return response
+
+@csrf_exempt
+def toggle_light(request):
+    try:
+        payload = json.loads(request.body.decode())
+        if(payload["light"] == "on"):
+            turnOn()
+        else:
+            turnOff()
+        response = JsonResponse({"success": "true"})
     except Exception as e:
         response = JsonResponse({"error": str(e)})
         response.status_code = 500
