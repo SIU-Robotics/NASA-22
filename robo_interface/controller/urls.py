6a3d84613326632546bcdbb3130f4dfabc1f319b
Andrew Barnes <barnesandrew123@gmail.com>
2023-01-17 21:25:24 -0600

    Revert "Added light code and gitignore file"
    This reverts commit 9f6a3d08765a50ab478f509576caeff03e401bf3.

--- robo_interface/controller/urls.py
+++ robo_interface/controller/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('test_page/', views.test_page, name='test_page'),
-	path('test/', views.test, name='test'),
-	path('toggle_light/', views.toggle_light, name='toggle_light'),
+	path('', views.index, name='index'),
+	path('test/', views.test, name='index')
 ]
 
